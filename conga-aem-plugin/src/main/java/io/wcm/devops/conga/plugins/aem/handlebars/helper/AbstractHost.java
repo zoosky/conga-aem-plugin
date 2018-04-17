372166e25269dd0644d068906b568cedfd6e09c0
Tobias Richter <trichter@pro-vision.de>
2018-04-17 14:43:56 +0200

    Add httpHost and httpHostSsl helper

---
+++ conga-aem-plugin/src/main/java/io/wcm/devops/conga/plugins/aem/handlebars/helper/AbstractHost.java
@@ -0,0 +1,55 @@
+package io.wcm.devops.conga.plugins.aem.handlebars.helper;
+
+import com.github.jknack.handlebars.Options;
+
+/**
+ * Abstract base class for host helpers
+ */
+abstract class AbstractHost {
+
+  /**
+   * Option for setting port
+   */
+  public static final String HASH_OPTION_PORT = "port";
+
+  /**
+   * Helper function to detect if the host already contains a port
+   *
+   * @param host The host
+   * @return true when the host already contains a port
+   */
+  private Boolean hasPort(String host) {
+    return host.matches("^.+:\\d+$");
+  }
+
+  /**
+   * Adds a port to the incoming host when it is not the default one
+   *
+   * @param context The context
+   * @param options Options
+   * @param defaultPort The default port that should not be added
+   * @return The hostname with port (when necessary)
+   */
+  Object addNonDefaultPort(Object context, Options options, Integer defaultPort) {
+    StringBuilder sb = new StringBuilder();
+    if (context == null) {
+      return null;
+    }
+    String host = context.toString();
+    // host has already a defined port
+    if (this.hasPort(host)) {
+      return context;
+    }
+    // retrieve the port, or take default one
+    Integer port = options.hash(HASH_OPTION_PORT, defaultPort);
+
+    // build result
+    sb.append(host);
+    if (! port.equals(defaultPort)) {
+      // add port when it is not the default one
+      sb.append(":").append(port);
+    }
+    return sb.toString();
+  }
+
+}
